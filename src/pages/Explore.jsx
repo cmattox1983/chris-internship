57a3a5c69995a581863edcd587e911f1ecefb608
cmattox1983 <chris@mattoxsfg.com>
2025-09-24 15:29:17 -0400

    WIP: Explore items + load-more

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from "react";
 import SubHeader from "../images/subheader.jpg";
 import ExploreItems from "../components/explore/ExploreItems";
+import axios from "axios";
 
 const Explore = () => {
   const [exploreItems, setExploreItems] = useState([]);
   const [loading, setLoading] = useState(true);
 
-async function fetchExploreItems(){
-  try{setLoading(true)
-    const {data}=await 
+  async function fetchExploreItems() {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
+      );
+      console.log(data);
+      setExploreItems(data);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
   }
-}
+
+  useEffect(() => {
+    fetchExploreItems();
+  }, []);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -41,7 +55,12 @@ async function fetchExploreItems(){
         <section aria-label="section">
           <div className="container">
             <div className="row">
-              <ExploreItems />
+              <ExploreItems
+                loading={loading}
+                items={exploreItems}
+                initial={8}
+                next={4}
+              />
             </div>
           </div>
         </section>
